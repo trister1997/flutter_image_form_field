a458af33052cf9ea097aa3ff3970521560a33de0
Tyler Rister <tyler.rister@gmail.com>
2019-05-05 19:04:16 -0400

    onRemove added

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -4,3 +4,4 @@ import 'package:flutter/widgets.dart' show Widget, BuildContext;
 typedef BuildImagePreviewCallback<T> = Widget Function(BuildContext, dynamic);
 typedef BuildButton = Widget Function(BuildContext, int);
 typedef InitializeFileAsImageCallback<T> = T Function(File);
+typedef RemoveImageCallback<T> = Function(dynamic);
